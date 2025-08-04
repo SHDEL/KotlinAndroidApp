2a106b07cc6be269e0281199cfae8f23bb1709d8
Deler <del2547.pv@gmail.com>
2025-08-04 21:35:57 +0700

    practice TaskUI

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -47,7 +48,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    GreetingImage("ไออ้วนไอสัส", "อ้วนตะวัน")
                     ArticleImageTutorial()
                 }
 
@@ -152,6 +152,83 @@ fun TaskManager(modifier: Modifier = Modifier){
 
     }
 }
+@Composable
+fun ComposeQuadrant(modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier.fillMaxWidth()
+
+    ) {
+        Row(Modifier.weight(1f)) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier.weight(1f).fillMaxSize().background(Color(0xFFEADDFF)).padding(16.dp)
+            ) {
+                Text(
+                    text = "Text composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Displays text and follows the recommended Material Design guidelines.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier.weight(1f).fillMaxSize().background(Color(0xFFD0BCFF)).padding(16.dp)
+            ) {
+                Text(
+                    text = "Image composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "Creates a composable that lays out and draws a given Painter class object.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+
+        }
+        Row(Modifier.weight(1f)) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier.weight(1f).fillMaxSize().background(Color(0xFFB69DF8)).padding(16.dp)
+            ) {
+                Text(
+                    text = "Row composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a horizontal sequence.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = modifier.weight(1f).fillMaxSize().background(Color(0xFFF6EDFF)).padding(16.dp)
+            ) {
+                Text(
+                    text = "Column composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a vertical sequence.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+
+        }
+
+    }
+
+
+}
 
 
 @Preview(
@@ -162,7 +239,7 @@ fun TaskManager(modifier: Modifier = Modifier){
 fun BirthdayCardPreview() {
     MyFirstApplicationTheme {
 //        GreetingText("Happy Birthday Del!", "Tawan")
-        GreetingImage("ไออ้วนไอสัส", "อ้วนตะวัน")
+        GreetingImage("Happy Birthday Na!", "Del")
     }
 }
 
@@ -185,6 +262,18 @@ fun ComposeArticlePreview(){
 fun TaskManagerPreview(){
     MyFirstApplicationTheme {
         TaskManager()
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun ComposeQuadrantPreview(){
+    MyFirstApplicationTheme {
+        ComposeQuadrant()
 
     }
 }
